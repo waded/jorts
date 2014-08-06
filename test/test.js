909006018d951b87457c652ad2dea543c15d736b
Wade Dorrell <wade@dorrells.org>
2014-08-06 01:07:19 -0600

    Rearrange tests in prep for additional spec

--- test/test.js
+++ test/test.js
@@ -12,21 +12,6 @@ test('Small numbers', function() {
 	many([3, 9], ['3', '9']);
 });
 
-test('Empty', function() {
-	many([], []);
-});
-
-test('Unexpected one', function() {	
-	one(undefined, undefined);
-	one("Fragrant Banana", undefined);
-});
-
-test("Unexpected many", function() {
-	many(undefined, undefined);
-	many("Palomino", undefined);
-	many({A:1}, undefined);	
-});
-
 test('Singles across the gamut', function() {
 	many([0], ['0']);
 	many([1], ['1']);
@@ -39,11 +24,24 @@ test('Singles across the gamut', function() {
 	many([1000000000], ['1B']);
 	many([10000000000], ['10B']);
 	many([100000000000], ['100B']);
+});
 
+test('Empty many', function() {
+	many([], []);
+});
+
+test('Unexpected values one', function() {	
+	one(undefined, undefined);
+	one("Fragrant Banana", undefined);
+});
+
+test("Unexpected values many", function() {
+	many(undefined, undefined);
+	many("Palomino", undefined);
+	many({A:1}, undefined);	
 });
 
 test('Jorts for negatives not well defined yet.', function() {
 	many([1000, -1000], ['1K', '-1K']);
 	many([1, -1000], ['0K', '-1K']);
-
 });
